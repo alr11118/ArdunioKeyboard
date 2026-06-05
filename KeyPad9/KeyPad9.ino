7c443f6ebde8b7d506a504c7c5290a3149ebd16a
alr11118 <alara.engin2008@gmail.com>
2026-06-04 22:12:26 -0700

    new file

---
+++ KeyPad9/KeyPad9.ino
@@ -0,0 +1,54 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
+
+const int rows[] = {2, 3, 4};
+const String rowsS[] = {"2", "0", "1"};
+const int cols[] = {5, 6, 7};
+const String colsS[] = {"2", "0", "1"};
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Version 4");
+
+  for (int r = 0; r < 3; r++) {
+    pinMode(rows[r], OUTPUT);
+    digitalWrite(rows[r], HIGH);
+  }
+
+  for (int c = 0; c < 3; c++) {
+    pinMode(cols[c], INPUT_PULLUP);
+  }
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (int r = 0; r < 3; r++) {
+
+    // Activate one row
+    digitalWrite(rows[r], LOW);
+    delay(1); // allow signal to settle
+
+    // Check every column
+    for (int c = 0; c < 3; c++) {
+      if (digitalRead(cols[c]) == LOW) {
+        Serial.print("Key pressed at row ");
+        Serial.print(rowsS[r]);
+        Serial.print(", column ");
+        Serial.println(colsS[c]);
+
+        // LCD Print
+        lcd.clear();
+        lcd.print("Row: " + rowsS[r]);
+        lcd.setCursor(0, 1);
+        lcd.print("Colunm: " + colsS[c]);
+
+        delay(100); // slow down output
+      }
+    }
+
+    // Deactivate row
+    digitalWrite(rows[r], HIGH);
+  }
+}
